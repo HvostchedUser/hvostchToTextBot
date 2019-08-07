0c98425ab295983b6b8791f26a11a55d9d7e4205
hvostchedUser <yogik_iz_gretsii@mail.ru>
2019-08-07 11:46:45 +0300

    standard commit

--- src/hvtotext.py
+++ src/hvtotext.py
@@ -51,6 +51,7 @@ def get_message():
 
 
 if __name__=='__main__':
+    os.makedirs("voice/")
     bot.remove_webhook()
     bot.set_webhook(url=os.getenv('WEBHOOK_URL')+API_TOKEN)
     server.run(host="0.0.0.0",
